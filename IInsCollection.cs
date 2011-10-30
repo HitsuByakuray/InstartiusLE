7426b8c78829c6c99e4a3c84bd6a6c0af5c717fe
Sergio Daniel Lozano Garcia <HitsuByakuray@gmail.com>
2011-10-30 16:53:44 -0500

    second commit

---
+++ IInsCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instartius_Language_Extender
+{
+    public interface IInsCollection <T>
+    {
+        bool IsEmpty();
+
+        void Contains(ref object obj);
+
+        int Size();
+
+        void Clear();
+
+        T[] ToArray();
+
+        string ToString();
+    }
+}
